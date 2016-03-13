6265dadf8b2d5ea34cd913c2dd842b3c47056016
Kevin Hooke <kevin.hooke@gmail.com>
2016-03-13 13:40:02 -0700

    added example2

--- src/main/java/kh/springproperties/propertyplaceholderexample/SpringPropertyPlaceholderExample.java
+++ src/main/java/kh/springproperties/propertyplaceholderexample/SpringPropertyPlaceholderExample.java
@@ -16,10 +16,14 @@ public class SpringPropertyPlaceholderExample {
 		ClassPathXmlApplicationContext ctx = 
 				new ClassPathXmlApplicationContext("application-context-propertyplaceholder.xml");
 		
+		// example1: using component-scan
 		ExampleBean bean = (ExampleBean)ctx.getBean("exampleBean");
-		System.out.println(bean.getProperty1());
-		System.out.println(bean.getProperty2());
+		System.out.println("example1: " + bean.getProperty1());
+		System.out.println("example2: " + bean.getProperty2());
 		
+		// example2: using explicit xml bean definitiion
+		ExampleBean2 bean2 = (ExampleBean2)ctx.getBean("exampleBean2");
+		System.out.println("example2: " + bean.getProperty1());
 	}
 	
 	
